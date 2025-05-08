94ae7012256832fd619065b9517677bef6ee869a
DhamodharanSubramanian <164064856+DhamodharanSubramanian@users.noreply.github.com>
2025-05-08 09:30:38 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (27.98%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
@@ -1,21 +1,17 @@
 int removeDuplicates(int* nums, int numsSize) {
     int ret_val = 1;
-    
-    unsigned int i,j;
 
-    if(numsSize > 1)
+    unsigned int i, j;
+
+    for (i = 1, j = 0; i < numsSize; i++) 
     {
-        for(i = 1, j=0 ; i < numsSize ; i++)
+        if (nums[i] > nums[j]) 
         {
-            if(nums[i] > nums[j])
-            {
-                j++;
-                nums[j] = nums[i];
-                ret_val++;
-            }
+            j++;
+            nums[j] = nums[i];
+            ret_val++;
         }
     }
 
     return ret_val;
-    
 }
