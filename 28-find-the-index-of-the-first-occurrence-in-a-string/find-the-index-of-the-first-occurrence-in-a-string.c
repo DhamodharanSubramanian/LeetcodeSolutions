8fd0f84aa06edd52ddac0ae93c297f299cba4478
DhamodharanSubramanian <164064856+DhamodharanSubramanian@users.noreply.github.com>
2025-05-08 14:21:09 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.15%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.c
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.c
@@ -3,7 +3,7 @@ int strStr(char* haystack, char* needle) {
     int found = -1;
     unsigned int i,j,temp;
 
-    for(i = 0 ; i < strlen(haystack); i++)
+    for(i = 0 ; i < strlen(haystack) ; i++)
     {
         temp = i;
         for(j = 0 ; j < strlen(needle) ; j++, temp++)
