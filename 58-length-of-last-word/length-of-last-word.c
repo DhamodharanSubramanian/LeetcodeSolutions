b26cb7be0bc8992f614f47cacbcf3819a3331494
DhamodharanSubramanian <164064856+DhamodharanSubramanian@users.noreply.github.com>
2025-05-09 10:57:41 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.79%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.c
@@ -0,0 +1,29 @@
+#define FALSE ((bool) 0u)
+#define TRUE ((bool) 1u)
+
+int lengthOfLastWord(char* s) {
+    unsigned int length = strlen(s);
+    int i;
+    bool word_found = FALSE;
+
+    int count = 0;
+
+    for ( i = length-1 ; i >= 0 ; i--)
+    {
+        if(!word_found)
+        {
+            if(s[i] != ' ')
+                word_found = TRUE;
+        }
+        
+        if(word_found == TRUE)
+        {
+            if(s[i] == ' ')
+            break;
+
+            count++;
+        }
+    }
+    
+    return count;
+}
